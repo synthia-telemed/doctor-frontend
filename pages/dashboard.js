a3162bb8b9051aae6a17983d508029769a81b007
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-10-01 14:25:19 +0700

    [SYN-212] Fix/resume video call and join again (#7)
    * disable dashboard fetching
    
    * move socket connect to useEffect
    
    * fix: re connection problem
    
    * turn off start-peering
    
    * remove default roomid

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,7 +3,6 @@ import useSWR from 'swr'
 import { apiFetcher } from '../api/axios'
 
 const Dashboard = () => {
-  const { appointments, error } = useSWR('/appointment/today', apiFetcher)
   // TODO: Handler error and display appointments
   return <div>Dashboard</div>
 }
